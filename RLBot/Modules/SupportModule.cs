691353f54cda5383eb0665f053081959b48e77c4
tomjan <tom_janssen_007@hotmail.com>
2017-12-21 14:36:54 +0100

    - Removed the cooldowns for now - Removed unneeded code - Changed the layout and automatically adds reactions now

--- RLBot/Modules/SupportModule.cs
+++ RLBot/Modules/SupportModule.cs
@@ -1,8 +1,6 @@
 ﻿using System.Threading.Tasks;
 using Discord;
 using Discord.Commands;
-using Discord.WebSocket;
-using RLBot.Preconditions;
 
 namespace RLBot.Modules
 {
@@ -10,51 +8,46 @@ namespace RLBot.Modules
     [Summary("Bot support")]
     public class SupportModule : ModuleBase<SocketCommandContext>
     {
-        private readonly DiscordSocketClient _client;
-
-        public SupportModule(DiscordSocketClient client)
-        {
-            _client = client;
-        }
-
         [Command("suggestion")]
-        [Cooldown(12, 0, 0)]
-        [Summary("Send in suggestions for new features or improvements for the bot. (12h cooldown)")]
+        [Summary("Send in suggestions for new features or improvements for the bot.")]
         [Remarks("suggestion <your suggestion>")]
         public async Task SuggestionAsync([Remainder] string suggestion)
         {
             if (suggestion == "") return;
 
-            var channel = _client.GetChannel(385368298381901842) as IMessageChannel;
+            var channel = Context.Client.GetChannel(393390897573789699) as IMessageChannel;
             if (channel == null)
             {
-                var application = await _client.GetApplicationInfoAsync();
+                var application = await Context.Client.GetApplicationInfoAsync();
                 await ReplyAsync($"Can't find the suggestion channel, please contact {application.Owner}.");
                 return;
             }
 
-            await channel.SendMessageAsync("", embed: new EmbedBuilder()
+            var msg = await channel.SendMessageAsync("", embed: new EmbedBuilder()
                 .WithColor(RLBot.EMBED_COLOR)
-                .WithAuthor(Context.Message.Author)
+                .WithTitle("Suggestion")
                 .WithDescription(suggestion.Trim())
-                .WithCurrentTimestamp()
+                .WithFooter($"Submitted by {Context.Message.Author}")
                 .Build());
+            
+            var e1Task = msg.AddReactionAsync(new Emoji("✅"));
+            var e2Task = msg.AddReactionAsync(new Emoji("❌"));
+            await Task.WhenAll(e1Task, e2Task); 
 
             await ReplyAsync("Succesfully submitted the suggestion.");
         }
 
         [Command("bugreport")]
-        [Cooldown(12, 0, 0)]
-        [Summary("Send in a detailed bugreport for the bot. Try to describe how to reproduce it if possible. (12h cooldown)")]
+        [Summary("Send in a detailed bugreport for the bot. Try to describe how to reproduce it if possible.")]
         [Remarks("bugreport <your bugreport>")]
         public async Task BugReportAsync([Remainder] string bug)
         {
             if (bug == "") return;
 
-            var channel = _client.GetChannel(387560503641374730) as IMessageChannel;
+            var channel = Context.Client.GetChannel(387560503641374730) as IMessageChannel;
             if (channel == null)
             {
-                var application = await _client.GetApplicationInfoAsync();
+                var application = await Context.Client.GetApplicationInfoAsync();
                 await ReplyAsync($"Can't find the bug report channel, please contact {application.Owner}.");
                 return;
             }
